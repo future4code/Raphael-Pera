c34f68dbcc7b45dfb2a1e0874bedf4544fe0af87
RaphaelRPera <raphael.r.pera@gmail.com>
2020-08-07 12:22:47 -0300

    interpretação EX1

---
+++ semana4/lista-de-exercicios/s4a5-exercicios.js
@@ -0,0 +1,29 @@
+
+  // INTERPRETAÇÃO DE CÓDIGO - 1) *********************************************************************
+    //O que ele faz:
+        /*  O código converte o valor de U$ 100 (cem dólares) para reais (R$) utilizando o valor
+            da cotação informado pelo usuário em dólares (U$) e
+            imprime o resultado no console */
+
+    //Como ele faz:
+        /*  Ao ser executado o código define a variável "meuDinheiro" que conterá o valor retornado
+            pela função "conversorDeMoeda". Com essa definição a função mencionada é invocada.
+            
+            A função "conversorDeMoeda", que recebe como único parâmetro o número 100 atribuído à
+            variável "valorEmDolar",  primeiramente solicita que o usuário digite o valor da
+            cotação do dólar em um "prompt" e atribui esse valor informado à constante "cotacao" em
+            formato de número. 
+            A função então retorna o valor resultante da multiplicação do valor de "cotacao" pelo
+            valor de "valorEmDolar", precedido do símbolo da modeda Real (R$).
+            
+            Por fim, o código imprime no console, através da função "console.log", o valor da
+            constante "meuDinheiro". */
+    
+    //O que é impresso no console:
+        /*  Será impresso o valor contido na constante "meuDinheiro", em formato de "string" */
+  //_______________________________________________________________________________________________
+
+
+
+
+
