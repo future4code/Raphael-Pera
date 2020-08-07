52d2662c85cc51c6dec5ce1e7a89fc2e33f1fc98
RaphaelRPera <raphael.r.pera@gmail.com>
2020-08-07 12:23:55 -0300

    interpretação EX2

--- semana4/lista-de-exercicios/s4a5-exercicios.js
+++ semana4/lista-de-exercicios/s4a5-exercicios.js
@@ -24,6 +24,33 @@
   //_______________________________________________________________________________________________
 
 
+  
+  // INTERPRETAÇÃO DE CÓDIGO - 2) *********************************************************************
+    //O que ele faz:
+        /*  O código calcula o valor total de dois montantes de dinheiro com base no tipo de
+            investimento e no valor investido, e imprime o valor dos montantes no console. */
+    
+    //Como ele faz:
+        /*    O código define as constantes "novoMontante" e "segundoMontante" que conterão os
+            valores totais dos investimentos.
+              Cada uma das duas constantes invoca a função "investeDinheiro" passando os parâmetros
+            as variáveis "tipoDeInvestimento", do tipo "string", e "valor", do tipo "number".
+              A função então define o cálculo a ser realizado utilizando uma condicional "switch/case"
+            que verifica qual o calculo adequado para o parâmetro "tipoDeinvestimento" recebido.
+              Com o cálculo encontrado, a função multiplica valor do parâmetro "valor" por um número
+            fixo determinado para aquele tipo de investimento, e atribui o resultado à variável
+            "valorApósInvestimento"
+              A função então retorna o valor de "valorAposInvestimento"
+              Por fim, o código imprime duas linhas no console, utilizando a função "console.log",
+            contendo os valores de "novoMontante" e "segundoMontante" respectivamente.
+              Caso a função não encontre o calculo adequado para o "tipoDeInvestimento" informado,
+            um alerta é emitido com a mensagem "TIPO DE INVESTIMENTO INFORMADO INCORRETO!", utilizando
+            "alert()". Nesse caso a variável "valorAposInvestimento" não é retornada pela função e
+            então o console imprimirá 2 linhas contendo o texto "undefined" */
 
+    //O que é impresso no console:
+        /*  165
+            TIPO DE INVESTIMENTO INFORMADO INCORRETO! */
+  //__________________________________________________________________________________________________
 
 
