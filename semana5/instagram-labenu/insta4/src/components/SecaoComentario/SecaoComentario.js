d8412d8a262f251c2c8fce48a040cdce06ffcc4c
RaphaelRPera <raphael.r.pera@gmail.com>
2020-08-12 16:41:27 -0300

    exercicios resolvidos

---
+++ semana5/instagram-labenu/insta4/src/components/SecaoComentario/SecaoComentario.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react'
+import './SecaoComentario.css'
+
+export class SecaoComentario extends Component {
+	state = {
+		inputValue: "",
+	}
+
+	onChangeComentario = (event) => {
+		this.setState({ inputValue: event.target.value })
+		console.log(`inputValue: ${this.state.inputValue}`)
+	}
+
+	render() {
+		return (
+			<div className={'comment-container'}>
+				<input
+					className={'input-comentario'}
+					placeholder={'Comentário'}
+					value={this.state.inputValue}
+					onChange={this.onChangeComentario}
+				/>
+				<button onClick={this.props.aoEnviar}>Enviar</button>
+			</div>
+		)
+	}
+}
