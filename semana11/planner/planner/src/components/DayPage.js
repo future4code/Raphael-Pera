72365f31375173c60e3b7a0519aee963cb7e6031
RaphaelRPera <raphael.r.pera@gmail.com>
2020-09-24 17:05:16 -0300

    inclusão e exclusão de tarefas funcionando

--- semana11/planner/planner/src/components/DayPage.js
+++ semana11/planner/planner/src/components/DayPage.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
+import axios from 'axios'
+import { baseUrl } from './APIData'
 
 const Box = styled.div`
     border: 1px solid black;
@@ -19,17 +21,100 @@ const TaskBox = styled.ul`
 const TaskLine = styled.li`
     margin: 0;
     padding: 5px;
+    /* display: flex;
+    justify-content: space-between;
+    align-items: center; */
+
+    display: grid;
+    grid-template-columns: 1fr 25px;
+`
+
+const TaskDelBtn = styled.span`
+    padding: 1px 3px;
+    margin-left: 5px;
+    color: red;
+    :hover{background-color: rgb(245,245,245);}
+    cursor: pointer;
+    text-align: center;
 `
 
 
-export const DayPage = () => {
+export const DayPage = (props) => {
+    // const [taskList, setTaskList] = useState([])
+
+    useEffect(()=>{
+        // if (props.taskList.length > 0) {
+        //     // setTaskList(props.taskList)
+            console.log('DayPage > useEffect > props.day:', props.day)
+            console.log('DayPage > useEffect > props.taskList:', props.taskList,)
+            // console.log('DayPage > useEffect > taskList2:', taskList)
+        //     verificaTaskList()
+        // }
+    },[props.taskList])
+
+
+    const dayName = (dayNum) => {
+        switch (dayNum) {
+            case 1: return 'Domingo'; break;
+            case 2: return 'Segunda'; break;
+            case 3: return 'Terça'; break;
+            case 4: return 'Quarta'; break;
+            case 5: return 'Quinta'; break;
+            case 6: return 'Sexta'; break;
+            case 7: return 'Sábado'; break;
+            default: return 'Diversos'; break;
+        }
+    } 
+
+    // const deleteTask = (task) => {
+    //     axios.delete(`${baseUrl}/${task.id}`)
+    //         .then(response=>{console.log(response)})
+    //         .catch(err=>{console.log(err)})
+    // }
+
+    // const verificaTaskList =()=>{
+    //     console.log('verificaTaskList:', props.taskList)
+    //     props.taskList.forEach(task => {
+    //         console.log('verificaTaskList:', task.id, Number(task.day), task.text)
+    //     });
+
+    //     // const novaLista = props.taskList.map(task=>{
+    //     //     return (
+    //     //         task.day === props.day && task.text
+    //     //     )
+    //     // })
+    //     // console.log('verificaTaskList:', novaLista)
+    // }
+
     return (
         <Box>
-            <Header>Dia da Semana</Header>
+            {/* <Header>Dia da Semana</Header> */}
+            <Header>{dayName(props.day)} </Header>
             <TaskBox>
-                <TaskLine>Tarefa 1</TaskLine>
-                <TaskLine>Tarefa 2</TaskLine>
-                <TaskLine>Tarefa 3</TaskLine>
+                <TaskLine>{props.day !== '' && 'Tarefa Inicial'}</TaskLine>
+                {props.taskList.length > 0 &&
+                    props.taskList.map(task=>{
+                        return (
+                            Number(task.day) === props.day &&
+                            <TaskLine key={task.id}>
+                                {task.text}
+                                <TaskDelBtn onClick={()=>props.deleteTask(task)}>x</TaskDelBtn>
+                            </TaskLine>
+                        )
+                    })
+                }
+                {props.day === '' &&
+                    props.taskList.length > 0 &&
+                        props.taskList.map(task=>{
+                            return (
+                                task.day === props.day &&
+                                <TaskLine key={task.id}>
+                                    {task.text}
+                                    <TaskDelBtn onClick={()=>props.deleteTask(task)}>x</TaskDelBtn>
+                                </TaskLine>
+                            )
+                        })
+                }
             </TaskBox>
         </Box>
     )
--- semana11/planner/planner/src/components/HeaderTop.js
+++ semana11/planner/planner/src/components/HeaderTop.js
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import styled from 'styled-components'
+import axios from 'axios'
+import { baseUrl } from './APIData'
 
 
-export const HeaderTop = () => {
+const Box = styled.header`
+
+`
+
+const Input = styled.input``
+
+const Select = styled.select``
+
+const Btn = styled.button``
+
+
+export const HeaderTop = (props) => {
+    const [inputValue, setInputValue] = useState('')
+    const [selectValue, setSelectValue] = useState(1)
+
+
+    // const criarTarefa = () => {
+    //     const body = {text: inputValue,day: selectValue,}
+    //     console.log(body)
+    //     axios.post(baseUrl, body)
+    //         .then(response=>{console.log(response)})
+    //         .catch(err=>{console.log(err)})
+    // }
+
+
+    const inputChange = (event) => {
+        setInputValue(event.target.value)
+        // console.log('HeaderTop > inputChange', event.target.value)
+    }
+
+    const selectChange = (event) => {
+        setSelectValue(event.target.value)
+        // console.log('HeaderTop > selectChange', event.target.value)
+    }
+
     return (
-        <div>Hello HeaderTop</div>
+        <Box>
+            <Input
+                placeholder={'Descrição da tarefa'}
+                onChange={inputChange}
+                value={inputValue}
+            />
+            <Select onChange={selectChange} value={selectValue}>
+                <option value={'1'}>Domingo</option>
+                <option value={'2'}>Segunda</option>
+                <option value={'3'}>Terça</option>
+                <option value={'4'}>Quarta</option>
+                <option value={'5'}>Quinta</option>
+                <option value={'6'}>Sexta</option>
+                <option value={'7'}>Sábado</option>
+            </Select>
+            <Btn onClick={()=>props.createTask(inputValue, selectValue) } >Criar Tarefa</Btn>
+        </Box>
     )
 }
