202db8c0fa5db976afbba5f2726b4e718bb6a041
Raphael <raphael.r.pera@gmail.com>
2020-10-20 18:36:46 -0300

    desafios 6 e 7 concluídos

--- semana14/aula2/exercicios/src/ex6.ts
+++ semana14/aula2/exercicios/src/ex6.ts
@@ -1,13 +1,8 @@
-const defineIdadeHistorica = (ano: number, sigla?: string): string => {
+const defineIdadeHistorica = (ano: number, sigla?: string | undefined): string => {
+    if (sigla !== 'AC' && sigla !== 'DC' && sigla !== undefined ) {return 'Sigla inválida!'}
+    if (isNaN(ano)) {return 'Ano inválido'}
     sigla = sigla || 'DC'
-    let idade
-    console.log(sigla)
-
-    //  Pré-historia
-    //  4000 AC - Idade Antiga
-    //  476 DC - Idade Média
-    //  1473 DC - Idade Moderna
-    //  1789 DC - Idade Contemporânea
+    let idadeH: string
 
     const anoAtual = new Date().getFullYear()
     const qtdeAnos = sigla === 'DC' ? anoAtual - ano : anoAtual + ano
@@ -18,9 +13,21 @@ const defineIdadeHistorica = (ano: number, sigla?: string): string => {
     const idadeAntiga: number = anoAtual + 4000
     const idadePreH: number = anoAtual + 10000
 
-    if (qtdeAnos >= idadeContemporanea) {
-
+    if (qtdeAnos >= idadePreH) {
+        idadeH = 'Pré-história'
+    } else if (qtdeAnos >= idadeAntiga) {
+        idadeH = 'Idade Antiga'
+    } else if (qtdeAnos >= idadeMedia) {
+        idadeH = 'Idade Media'
+    } else if (qtdeAnos >= idadeModerna) {
+        idadeH = 'Idade Moderna'
+    } else if (qtdeAnos >= idadeContemporanea) {
+        idadeH = 'Idade Contemporânea'
+    } else {
+        idadeH = 'ERRO NA IDADE'
     }
 
-    return ''
+    return idadeH
 }
+
+console.log(defineIdadeHistorica(11000, 'AC'))
---
+++ semana14/aula2/exercicios/src/ex7.ts
@@ -0,0 +1,52 @@
+type product = {
+    name: string,
+    price: number,
+    classification: string,
+}
+
+const produtos: product[] = [
+    {
+        name: 'Blusinha',
+        price: 30,
+        classification: 'verão',
+    },
+
+    {
+        name: 'Moletom',
+        price: 100,
+        classification: 'inverno',
+    },
+    
+    {
+        name: 'Toalha',
+        price: 50,
+        classification: 'banho',
+    },
+    
+    {
+        name: 'Cueca',
+        price: 20,
+        classification: 'intimas',
+    },
+]
+
+
+const blackFridayPricing = (products: product[]): [] | {} => {
+    const newProducts = products.map(prod=>{
+        let discountPrice: number | string
+        switch (prod.classification) {
+            case "verão": discountPrice = prod.price * (1 - (5/100)); break;
+            case "inverno": discountPrice = prod.price * (1 - (10/100)); break;
+            case "banho": discountPrice = prod.price * (1 - (4/100)); break;
+            case "intimas": discountPrice = prod.price * (1 - (7/100)); break;
+            default: discountPrice = 0/100; break;
+        }
+
+        return {...prod, discountPrice: Number(discountPrice.toFixed(2))}
+    })
+
+    // console.table(newProducts)
+    return newProducts
+}
+
+console.table(blackFridayPricing(produtos))
