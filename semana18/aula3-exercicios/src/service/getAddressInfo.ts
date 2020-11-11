e7d5198bfd3058741ddaeea645d289ab820a1857
RaphaelRPera <raphael.r.pera@gmail.com>
2020-11-11 17:20:49 -0300

    função getAddressInfo criada

---
+++ semana18/aula3-exercicios/src/service/getAddressInfo.ts
@@ -0,0 +1,22 @@
+import axios from 'axios'
+
+const URL_ADDRESS = "https://viacep.com.br/ws"
+
+type address = {
+    logradouro: string,
+    bairro: string,
+    cidade: string,
+    estado: string
+}
+
+export async function getAddressInfo(cep: string): Promise<address> {
+    const cepData: any = await axios.get(`${URL_ADDRESS}/${cep}/json`)
+    const {logradouro, bairro, localidade, uf} = cepData.data
+    
+    const result = {logradouro, bairro, cidade: localidade, estado: uf}
+    console.log(result)
+
+    return result
+}
+
+getAddressInfo('02516000')
