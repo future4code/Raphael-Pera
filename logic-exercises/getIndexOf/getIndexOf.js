70d08a390a37a1ef8ff58320a4c0f056e938d414
RaphaelRPera <raphael.r.pera@gmail.com>
2021-01-12 18:06:12 -0300

    exercício de lógica resolvido

---
+++ logic-exercises/getIndexOf/getIndexOf.js
@@ -0,0 +1,9 @@
+function getIndexOf(source, character) {
+    var sourceArray = source.split('');
+    for (var i = 0; i < sourceArray.length; i++) {
+        if (sourceArray[i] === character)
+            return i;
+    }
+    return -1;
+}
+console.log(getIndexOf('jabuticaba', 'j'));
---
+++ logic-exercises/getIndexOf/getIndexOf.ts
@@ -0,0 +1,9 @@
+function getIndexOf(source: string, character: string):number | any {
+    const sourceArray = source.split('')
+    for (let i = 0; i < sourceArray.length; i++){
+        if (sourceArray[i] === character) return i 
+    }
+    return -1
+}
+
+console.log(getIndexOf('jabuticaba', 'j'))
